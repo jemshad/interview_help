3bc55b6cc355ab1338742a085a84b9301fe022ea
Jemshad O K <jemshad.ok@inmobi.com>
2018-08-14 17:31:03 +0530

    single pass version of substring match

--- easy/substring.py
+++ easy/substring.py
@@ -13,3 +13,13 @@ def substring(str1, str2):
       return True
   return False
 
+
+# with single loop
+
+def str_search(str1, str2):
+    for i in range(len(str2) - len(str1) + 1):
+        if str1 == str2[i:i+len(str1)]:
+            return True
+    else:
+        return False
+
